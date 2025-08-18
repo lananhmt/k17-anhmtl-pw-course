c91c9129dd6fe582341507ef1dd1eedf405cdefc
lananh <anh.lanbk99@gmail.com>
2025-08-18 20:06:36 +0700

    chore: update lesson-2/ex4.js

--- tests/lesson-2/ex4.js
+++ tests/lesson-2/ex4.js
@@ -1,5 +1,11 @@
 const chieuCao = 152;
 
+let canNangLyTuong = (chieuCao - 100) * 9 / 10;
+
+let canNangToiDa = chieuCao - 100;
+
+let canNangToiThieu = (chieuCao - 100) * 8 / 10;
+
 console.log("Cân nặng lý tưởng, Cân nặng tối đa, Cân nặng tối thiểu lần lượt là: ");
 
-console.log((chieuCao - 100) * 9 / 10, (chieuCao - 100), (chieuCao - 100) * 8 / 10);
+console.log(canNangLyTuong + ', ' + canNangToiDa + ', ' + canNangToiThieu);
